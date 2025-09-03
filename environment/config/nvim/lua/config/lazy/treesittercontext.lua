2432575fd2e9a05e8b036f7bd4e4518ea566bb39
Harry Thompson <harrythompson3765@gmail.com>
2025-09-03 15:51:17 +0100

    nvim context disbaled on start

--- environment/config/nvim/lua/config/lazy/treesittercontext.lua
+++ environment/config/nvim/lua/config/lazy/treesittercontext.lua
@@ -2,6 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
     config = function()
         require('treesitter-context').setup({
+            enable = false,
             max_lines = 1
         })
         vim.keymap.set("n", "<leader>C", "<cmd>TSContext toggle<CR>")
