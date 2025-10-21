6feceb8edcc3890b31cc6ac820589bdd5b7f559e
Harry Thompson <harrythompson3765@gmail.com>
2025-10-21 20:32:44 +0100

    293 dms

--- flake.nix
+++ flake.nix
@@ -23,9 +23,21 @@
             url = "github:vicinaehq/vicinae";
         };
 
+        dgop = {
+            url = "github:AvengeMedia/dgop";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        dms-cli = {
+            url = "github:AvengeMedia/danklinux";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
         dankMaterialShell = {
             url = "github:AvengeMedia/DankMaterialShell";
             inputs.nixpkgs.follows = "nixpkgs";
+            inputs.dgop.follows = "dgop";
+            inputs.dms-cli.follows = "dms-cli";
         };
     };
 
