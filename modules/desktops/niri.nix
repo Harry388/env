c0f670f34ee93745aa66e4a01e1b86cd31692d5c
Harry Thompson <harrythompson3765@gmail.com>
2025-11-05 19:47:18 +0000

    97 xdg portal

--- modules/desktops/niri.nix
+++ modules/desktops/niri.nix
@@ -11,10 +11,11 @@
 
     xdg.portal = {
         enable = true;
+        wlr.enable = true;
         xdgOpenUsePortal = true;
-        extraPortals = [
-            pkgs.xdg-desktop-portal-gtk
-            pkgs.xdg-desktop-portal-gnome
+        extraPortals = with pkgs; [
+            xdg-desktop-portal-gtk
+            xdg-desktop-portal-gnome
         ];
     };
 
