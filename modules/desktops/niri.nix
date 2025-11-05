5205369700b21f7abe3a06eebf352316ab1416b3
Harry Thompson <harrythompson3765@gmail.com>
2025-11-05 20:11:24 +0000

    99 xdg portal

--- modules/desktops/niri.nix
+++ modules/desktops/niri.nix
@@ -17,15 +17,15 @@
             xdg-desktop-portal-gtk
             xdg-desktop-portal-gnome
         ];
-        config.niri = {
-            default = ["gnome" "gtk"];
-            "org.freedesktop.impl.portal.Access" = "gtk";
-            "org.freedesktop.impl.portal.Notification" = "gtk";
-            "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
-            "org.freedesktop.impl.portal.FileChooser" = "gtk";
-            "org.freedesktop.impl.portal.ScreenCast" = "gnome";
-            "org.freedesktop.portal.ScreenCast" = "gnome";
-        };
+        # config.niri = {
+        #     default = ["gnome" "gtk"];
+        #     "org.freedesktop.impl.portal.Access" = "gtk";
+        #     "org.freedesktop.impl.portal.Notification" = "gtk";
+        #     "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+        #     "org.freedesktop.impl.portal.FileChooser" = "gtk";
+        #     "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+        #     "org.freedesktop.portal.ScreenCast" = "gnome";
+        # };
     };
 
     environment.sessionVariables = {
