944dbef9c7494f37db4f6ec037d157bfb9952509
Harry Thompson <harrythompson3765@gmail.com>
2025-12-27 20:04:42 +0000

    123 spicetify

--- homeManager/spicetify.nix
+++ homeManager/spicetify.nix
@@ -10,7 +10,7 @@ in util.mkModule {
 
     programs.spicetify = {
         enable = true;
-        theme = spicePkgs.themes.catppuccin;
+        theme = spicePkgs.themes.defaultDynamic;
     };
 
 }
