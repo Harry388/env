73a89009254c71c31ee9be15a67dbc9707632041
Harry Thompson <harrythompson3765@gmail.com>
2026-01-15 10:53:59 +0000

    battery comment

--- modules/battery.nix
+++ modules/battery.nix
@@ -19,7 +19,7 @@
             CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
             CPU_MIN_PERF_ON_AC = 0;
-            CPU_MAX_PERF_ON_AC = 100;
+            CPU_MAX_PERF_ON_AC = 100; # Might need to lower this if overheating is a problem
             CPU_MIN_PERF_ON_BAT = 0;
             CPU_MAX_PERF_ON_BAT = 20;
 
