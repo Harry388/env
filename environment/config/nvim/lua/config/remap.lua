6ca4e23c1990be4fd08d2c0b4087d6ecf2b6f26f
Harry Thompson <harrythompson3765@gmail.com>
2026-06-11 17:41:53 +0100

    nvim quickfix binds

--- environment/config/nvim/lua/config/remap.lua
+++ environment/config/nvim/lua/config/remap.lua
@@ -2,16 +2,16 @@ vim.g.mapleader = " "
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("n", "<leader>t", "<cmd>tabnew<CR>")
+vim.keymap.set({"n", "v"}, "<leader>c", [["+]])
+vim.keymap.set({"n", "v"}, "<leader>v", [["_]])
+vim.keymap.set("x", "<C-j>", "<cmd>m +1<CR><Esc><S-v>")
+vim.keymap.set("x", "<C-k>", "<cmd>m -2<CR><Esc><S-v>")
 vim.keymap.set("n", "<C-l>", "<cmd>tabnext<CR>")
 vim.keymap.set("n", "<C-h>", "<cmd>tabprev<CR>")
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<C-k>", "<cmd>cprevious<CR>")
 vim.keymap.set("n", "<leader>qq", "<cmd>copen<CR>")
-vim.keymap.set({"n", "v"}, "<leader>c", [["+]])
-vim.keymap.set({"n", "v"}, "<leader>v", [["_]])
-vim.keymap.set("x", "<C-j>", "<cmd>m +1<CR><Esc><S-v>")
-vim.keymap.set("x", "<C-k>", "<cmd>m -2<CR><Esc><S-v>")
-vim.keymap.set("n", "<leader>bm", function ()
+vim.keymap.set("n", "<leader>qm", function ()
     local bufs = vim.api.nvim_list_bufs()
     local loaded_modified_bufs = {}
     local count = 1
@@ -29,5 +29,9 @@ vim.keymap.set("n", "<leader>bm", function ()
         end
     end
     vim.fn.setqflist({}, "r", { title = "Modified Buffers", items = loaded_modified_bufs })
+    vim.cmd("copen")
+end)
+vim.keymap.set("n", "<leader>qe", function ()
+    vim.diagnostic.setqflist()
     vim.cmd("copen")
 end)
